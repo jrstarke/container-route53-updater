98bc2c47316f5d7f4b421d1414448540337e8a0c
Jamie Starke <jamies@giftbit.com>
2017-01-18 14:20:16 -0800

    Set up environment variables, and set the script to updated the ip address on start

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM alpine:latest
 
+ENV PUBLIC_IP_HOSTED_ZONE=""
+ENV PUBLIC_IP_DOMAIN=""
+ENV LOCAL_IP_HOSTED_ZONE=""
+ENV LOCAL_IP_DOMAIN=""
+
 RUN apk -Uuv add curl bash python py-pip && \
     pip install awscli && \
     apk --purge -v del py-pip && \
--- startup.sh
+++ startup.sh
@@ -13,11 +13,51 @@ function clean_up {
 
 trap clean_up SIGTERM
 
-public_ip=$(curl $META_DATA_HOST/latest/meta-data/public-ipv4)
-private_ip=$(curl $META_DATA_HOST/latest/meta-data/local-ipv4)
+if [[ -n "$PUBLIC_IP_HOSTED_ZONE" ]] && [[ -n "$PUBLIC_IP_DOMAIN" ]]; then
+    public_ip=$(curl -f $META_DATA_HOST/latest/meta-data/public-ipv4)
 
-echo "public_ip: $public_ip"
-echo "private_ip: $private_ip"
+    if [[ -n "$public_ip" ]]; then
+        route53 change-resource-record-sets --hosted-zone-id $PUBLIC_IP_HOSTED_ZONE --change-batch "{
+            \"Changes\": [
+                {
+                    \"Action\": \"UPSERT\",
+                    \"ResourceRecordSet\": {
+                        \"Name\": \"$PUBLIC_IP_DOMAIN\",
+                        \"Type\": \"A\",
+                        \"TTL\": 60, \"ResourceRecords\": [
+                            {
+                                \"Value\":\"$public_ip\"
+                            }
+                        ]
+                    }
+                }
+            ]
+        }"
+    fi
+fi
+
+if [[ -n "$LOCAL_IP_HOSTED_ZONE" ]] && [[ -n "$LOCAL_IP_DOMAIN" ]]; then
+    local_ip=$(curl -f $META_DATA_HOST/latest/meta-data/local-ipv4)
+
+    if [[ -n "$local_ip" ]]; then
+        route53 change-resource-record-sets --hosted-zone-id $LOCAL_IP_HOSTED_ZONE --change-batch "{
+            \"Changes\": [
+                {
+                    \"Action\": \"UPSERT\",
+                    \"ResourceRecordSet\": {
+                        \"Name\": \"$LOCAL_IP_DOMAIN\",
+                        \"Type\": \"A\",
+                        \"TTL\": 60, \"ResourceRecords\": [
+                            {
+                                \"Value\":\"$local_ip\"
+                            }
+                        ]
+                    }
+                }
+            ]
+        }"
+    fi
+fi
 
 # Wait for the machine to be terminated
 while true; do sleep 86400 & wait; done
